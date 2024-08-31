b77eedc83cc5de14e0470638a3e1dfb974812c42
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2024-08-31 15:16:01 +1000

    Update Lab3.R

--- Lab3.R
+++ Lab3.R
@@ -4,3 +4,9 @@ install.packages("MindOnStats")
 library(MindOnStats)
 data(HoldingBreath, package="MindOnStats")
 head(HoldingBreath)
+data(HoldingBreath)
+hist(HoldingBreath$Time, xlab = "Time (seconds)")
+boxplot(Time ~ Gender, data = HoldingBreath, horizontal = TRUE,
+        xlab = "Time (seconds)", main = "Holding breath time")
+plot(Time ~ Age, data=HoldingBreath, xlab = "Age (years)", ylab = "Time
+(seconds)", main = "Holding Breath, Time vs Age")
