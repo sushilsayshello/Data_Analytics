e27f8327f0a89eb8dcea01c845efdf61cb64b7b4
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2024-08-31 15:16:08 +1000

    Update Lab3.R

--- Lab3.R
+++ Lab3.R
@@ -10,3 +10,19 @@ boxplot(Time ~ Gender, data = HoldingBreath, horizontal = TRUE,
         xlab = "Time (seconds)", main = "Holding breath time")
 plot(Time ~ Age, data=HoldingBreath, xlab = "Age (years)", ylab = "Time
 (seconds)", main = "Holding Breath, Time vs Age")
+library(ggplot2)
+ggplot(HoldingBreath,
+       aes(x=Age,
+           y=Time,
+           color=Exercise))+
+  geom_point()
+p1 <-ggplot(HoldingBreath,
+            aes(x=Age,
+                y=Time,
+                color=Exercise))
+p1 + geom_point() + geom_smooth()
+p2 <-ggplot(HoldingBreath,
+            aes(x=Gender,
+                y=Time))
+p2 +geom_boxplot()
+p2 +geom_boxplot() +geom_point()
