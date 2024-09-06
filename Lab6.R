c5c03bdd087e78a04de4f14b00e3fabf29c6033d
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2024-09-06 20:13:03 +1000

    Update Lab6.R

--- Lab6.R
+++ Lab6.R
@@ -6,3 +6,8 @@ str(penguins)
 colSums(is.na(penguins))
 penguins <- na.omit(penguins)
 library(ggplot2)
+ggplot(penguins,aes(x=bill_length_mm,y=flipper_length_mm,colour=species
+))+geom_point(size=2)
+set.seed(42)
+cluster.penguins <- kmeans(x=penguins[,c(3,5)],centers=2,iter.max =
+                             50)
