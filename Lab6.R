bf05f853b86d6def70a8fc0ea32292cc2d77b2dd
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2024-09-06 20:13:10 +1000

    Update Lab6.R

--- Lab6.R
+++ Lab6.R
@@ -11,3 +11,14 @@ ggplot(penguins,aes(x=bill_length_mm,y=flipper_length_mm,colour=species
 set.seed(42)
 cluster.penguins <- kmeans(x=penguins[,c(3,5)],centers=2,iter.max =
                              50)
+cluster.penguins$cluster
+cluster.penguins$centers
+table(cluster.penguins$cluster,penguins$species)
+factors <- as.factor(cluster.penguins$cluster)
+ggplot(penguins,aes(x=bill_length_mm,y=flipper_length_mm,colour=factors
+))+geom_point(size=2)
+
+
+ggplot(penguins, aes(x=bill_length_mm, y=flipper_length_mm, colour=factors)) +
+  geom_point(size=2) +
+  scale_colour_manual(values=c("blue", "red")) 
