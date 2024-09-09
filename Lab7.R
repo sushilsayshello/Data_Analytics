1afe99d89690a64f4538b0a2c5dc7f6ea9195d4c
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2024-09-09 14:23:26 +1000

    Update Lab7.R

--- Lab7.R
+++ Lab7.R
@@ -3,4 +3,12 @@ install.packages('caret')
 install.packages('kernlab')
 library(caret)
 library(kernlab)
+# ================================
+# Naive Bayes on the Iris dataset
+# ================================
+
+# Load the iris dataset and separate attributes and labels
+data("iris")
+x = iris[,-5]  # Attributes
+y = iris$Species  # Labels
 
